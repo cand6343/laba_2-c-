edd6ad3d2588cfa2ca3477741aab9bc75f46bd49
cand66343 <fjhdjdj876@gmail.com>
2025-11-24 12:45:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"число {number} чётное");
+        }
+        else
+        {
+            Console.WriteLine($"число {number} нечётное");
+        }
+    }
+}
