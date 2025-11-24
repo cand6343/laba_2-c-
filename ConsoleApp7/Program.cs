c155ac91f7c963e88c957ece5876e0ba77ae6e34
cand66343 <fjhdjdj876@gmail.com>
2025-11-24 12:50:14 +0300

    3 задание

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,19 +4,21 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите пароль: ");
-        string password = Console.ReadLine();
+        Console.Write("Введите четырёхзначное число: ");
+        int number = int.Parse(Console.ReadLine());
 
-        Console.Write("Подтвердите пароль: ");
-        string confirmPassword = Console.ReadLine();
+        int num1 = number / 1000;
+        int num2 = (number / 100) % 10;
+        int num3 = (number / 10) % 10;
+        int num4 = number % 10;
 
-        if (password == confirmPassword)
+        if (num1 + num4 == num2 - num3)
         {
-            Console.WriteLine("Пароль принят");
+            Console.WriteLine("ДА");
         }
         else
         {
-            Console.WriteLine("Пароль не принят");
+            Console.WriteLine("НЕТ");
         }
     }
 }
