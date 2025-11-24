349c546d9eae9a94b0366425caf9bb15b51f0a98
cand66343 <fjhdjdj876@gmail.com>
2025-11-24 12:53:05 +0300

    5 задание

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,16 +4,19 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите ваш возраст: ");
-        int age = int.Parse(Console.ReadLine());
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
 
-        if (age >= 18)
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 < num2)
         {
-            Console.WriteLine("Доступ разрешен");
+            Console.WriteLine(num1);
         }
         else
         {
-            Console.WriteLine("Доступ запрещен");
+            Console.WriteLine(num2);
         }
     }
 }
